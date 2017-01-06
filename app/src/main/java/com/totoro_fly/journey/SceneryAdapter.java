3562a2785c61ecf5b20e83ed32fe860b0c1e760d
totoro-fly <liujian180389@gmail.com>
2017-01-07 03:20:43 +0800

    viewhorder

--- app/src/main/java/com/totoro_fly/journey/SceneryAdapter.java
+++ app/src/main/java/com/totoro_fly/journey/SceneryAdapter.java
@@ -21,20 +21,32 @@ public class SceneryAdapter extends ArrayAdapter {
         super(context, 0, resource);
     }
 
+    private static class ViewHolder {
+        ImageView imageView;
+        TextView titel;
+        TextView content;
+    }
+
     @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View itemView = convertView;
-        if (itemView == null)
-            itemView = LayoutInflater.from(getContext()).inflate(R.layout.scenery_list_item, parent,false);
+        ViewHolder viewHolder;
+        if (itemView == null) {
+            itemView = LayoutInflater.from(getContext()).inflate(R.layout.scenery_list_item, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.imageView = (ImageView) itemView.findViewById(R.id.item_image);
+            viewHolder.titel = (TextView) itemView.findViewById(R.id.item_title);
+            viewHolder.content = (TextView) itemView.findViewById(R.id.item_content);
+            itemView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
         Scenery scenery = (Scenery) getItem(position);
-        ImageView imageView = (ImageView) itemView.findViewById(R.id.item_image);
-        imageView.setImageResource(scenery.getmImage());
-        TextView titel = (TextView) itemView.findViewById(R.id.item_title);
-        titel.setText(scenery.getmTitel());
-        TextView content = (TextView) itemView.findViewById(R.id.item_content);
-        content.setText(scenery.getmContent());
+        viewHolder.imageView.setImageResource(scenery.getmImage());
+        viewHolder.titel.setText(scenery.getmTitel());
+        viewHolder.content.setText(scenery.getmContent());
         return itemView;
-        
+
     }
 }
